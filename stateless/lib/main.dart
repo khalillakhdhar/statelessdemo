fbc0b3feac7f3693dcfc630fd1148944f6fb8cf9
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-01 20:09:13 +0100

    Update main.dart

--- stateless/lib/main.dart
+++ stateless/lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MystatelessWidget());
+  runApp(new MaterialApp(home: MystatelessWidget()));
 }
 
 class MystatelessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(appBar: new AppBar(title: new Text("my super app")));
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("my super app"),
+      ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new Text("hello"),
+            new Text("good morning"),
+            new Text("good job"),
+          ],
+        ),
+      ),
+    );
   }
 }
